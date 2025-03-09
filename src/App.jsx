801111cacdaf0a5ef85d206aecb7189c380367e8
zuwhi <zuwhyyy@gmail.com>
2025-03-09 11:10:13 +0700

    cn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Products from "./pages/products";
-import Layout from "./layouts/layouts";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Layout from "./layouts/Layouts";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import ProtectedRoute from "./config/protected";
@@ -10,39 +10,17 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <Routes>
-    
       <Route path="login" element={<Login />} />
 
       <Route path="/" element={<Layout />}>
-        <Route
-          index
-          element={
-           
-              <Home />
-           
-          }
-        />
-        <Route
-          path="products"
-          element={
-           
-              <Products />
-         
-          }
-        />
-        <Route
-          path="product/:id"
-          element={
-       
-              <Product />
-           
-          }
-        />
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="product/:id" element={<Product />} />
         <Route
           path="/dashboard"
           element={
             <ProtectedRoute>
-              <Dashboard/>
+              <Dashboard />
             </ProtectedRoute>
           }
         />
